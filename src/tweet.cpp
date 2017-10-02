8104e97773355499054fab80f748e78cc42a64ca
zhangn5 <zhangni0420@gmail.com>
2017-10-02 14:34:36 -0400

    added tweet class

---
+++ src/tweet.cpp
@@ -0,0 +1,9 @@
+//
+//  tweet.cpp
+//  
+//
+//  Created by Ni Zhang on 10/2/17.
+//
+//
+
+#include "tweet.hpp"
---
+++ src/tweet.hpp
@@ -0,0 +1,14 @@
+//
+//  tweet.hpp
+//  
+//
+//  Created by Ni Zhang on 10/2/17.
+//
+//
+
+#ifndef tweet_hpp
+#define tweet_hpp
+
+#include <stdio.h>
+
+#endif /* tweet_hpp */
